794b691409b43f65e3d424c29c8f42be5febd53c
Paul Sinnett <paul.sinnett@gmail.com>
2025-03-28 08:29:18 +0000

    window layers

--- Assets/Game Loop Scripts/DragTab.cs
+++ Assets/Game Loop Scripts/DragTab.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class DragTab : MonoBehaviour
 {
-    private bool isDragging = false;
     private Vector3 offset;
-    public Collider2D dragRegion; // Assignni a specifc region for draging
-    public SpriteRenderer spriteRenderer;
+    [SerializeField] BoxCollider2D dragRegion; // Assignni a specifc region for draging
+    [SerializeField] Window window;
+
+    void Awake()
+    {
+        dragRegion = GetComponent<BoxCollider2D>();
+        if (!window)
+        {
+            Debug.LogWarning("Window not assigned to DragTab script. Assign it in the inspector.", gameObject);
+        }
+    }
+
+    public void SetPosition(Vector2 offset, Vector2 size)
+    {
+        dragRegion.offset = offset;
+        dragRegion.size = size;
+    }
 
     private void OnMouseDrag()
     {
         Vector3 newPosition = GetMouseWorldPos() + offset;
-        if (spriteRenderer)
-        {
-            spriteRenderer.transform.position = ClampToScreenBounds(newPosition); // Keep it within screen limits
-        }
+        window.Move(newPosition);
     }
 
     private void OnMouseDown()
     {
-        offset = spriteRenderer.transform.position - GetMouseWorldPos();
+        window.BringToTop();
+        offset = window.transform.position - GetMouseWorldPos();
     }
 
     private Vector3 GetMouseWorldPos()
@@ -28,27 +40,6 @@ public class DragTab : MonoBehaviour
         Vector3 mousePoint = Input.mousePosition;
         mousePoint.z = Camera.main.WorldToScreenPoint(transform.position).z;
         return Camera.main.ScreenToWorldPoint(mousePoint);
-    }
-
-    private Vector3 ClampToScreenBounds(Vector3 position)
-    {
-        Camera cam = Camera.main;
-        Vector3 minScreenBounds = cam.ViewportToWorldPoint(new Vector3(0, 0, 0)); // Bottom-left corner
-        Vector3 maxScreenBounds = cam.ViewportToWorldPoint(new Vector3(1, 1, 0)); // Top-right corner
-
-        float clampedX = position.x;
-        float clampedY = position.y;
-        if (spriteRenderer)
-        {
-            float objectWidth = spriteRenderer.bounds.extents.x;  // Half-width of the object
-            float objectHeight = spriteRenderer.bounds.extents.y; // Half-height of the object
-
-            // Clamp X and Y positions within screen bounds
-            clampedX = Mathf.Clamp(position.x, minScreenBounds.x + objectWidth, maxScreenBounds.x - objectWidth);
-            clampedY = Mathf.Clamp(position.y, minScreenBounds.y + objectHeight, maxScreenBounds.y - objectHeight);
-        }
-
-        return new Vector3(clampedX, clampedY, position.z);
     }
 }
 
--- Assets/UI scripts/For room level/OneHideForSprite.cs
+++ Assets/UI scripts/For room level/OneHideForSprite.cs
@@ -1,24 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class OneHideForSprite : MonoBehaviour
 {
     public GameObject objectttohide;
-    // Start is called before the first frame update
-    void Start()
+    BoxCollider2D boxCollider;
+
+    void Awake()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    public void SetPosition(Vector2 offset, Vector2 size)
+    {
+        if (boxCollider)
+        {
+            boxCollider.offset = offset;
+            boxCollider.size = size;
+        }
+        else
+        {
+            Debug.LogWarning($"BoxCollider2D is missing for '{objectttohide.name}'");
+        }
     }
 
     private void OnMouseDown()
     {
         objectttohide.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
